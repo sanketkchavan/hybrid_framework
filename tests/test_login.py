b1694d8c34910d044cc44777e550b5f3ff7717bb
sanketkchavan <chavan.sanket642@gmail.com>
2023-03-02 12:10:31 +0530

    Added header verification Test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from assertpy import assert_that
+from selenium.webdriver.common.by import By
 
 
 class TestLoginUi:
@@ -9,3 +11,11 @@ class TestLoginUi:
         driver.get("https://opensource-demo.orangehrmlive.com/")
         actual_title = driver.title
         assert actual_title == "OrangeHRM"
+
+    def test_header(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.implicitly_wait(20)
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        act_header = driver.find_element(By.XPATH, "//h5[normalize-space()='Login']").text
+        assert_that("Login").is_equal_to(act_header)
