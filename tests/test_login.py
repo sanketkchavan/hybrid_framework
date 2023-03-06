d912549921747845ef126aba44250f4cb746387d
sanketkchavan <chavan.sanket642@gmail.com>
2023-03-06 11:13:28 +0530

    Added Valid login test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,21 +1,41 @@
+import pytest
 from selenium import webdriver
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
 
-class TestLoginUi:
+class TestLogin:
+    @pytest.fixture(scope="function", autouse=True)
+    def browser_config(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
+    def test_valid_login(self):
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[contains(normalize-space(),'Login')]").click()
+        actual_text = self.driver.find_element(By.XPATH, "//h6[contains(normalize-space(),'Dashboard')]").text
+        assert_that("Dashboard").is_equal_to(actual_text)
+
+
+class TestLoginUI:
+    @pytest.fixture(scope="class", autouse=True)
+    def browser_config(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(20)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        yield
+        self.driver.quit()
+
     def test_title(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
-        actual_title = driver.title
-        assert actual_title == "OrangeHRM"
+        actual_title = self.driver.title
+        assert_that("OrangeHRM").is_equal_to(actual_title)
 
     def test_header(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-        driver.implicitly_wait(20)
-        driver.get("https://opensource-demo.orangehrmlive.com/")
-        act_header = driver.find_element(By.XPATH, "//h5[normalize-space()='Login']").text
-        assert_that("Login").is_equal_to(act_header)
+        actual_header = self.driver.find_element(By.XPATH, "//h5").text
+        assert_that("Login").is_equal_to(actual_header)
